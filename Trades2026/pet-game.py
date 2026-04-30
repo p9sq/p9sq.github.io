a08127e5d2e32afe639377606f4793bd310d5137
p9sq <62570697+p9sq@users.noreply.github.com>
2026-05-01 09:23:53 +1200

    Update

---
+++ Trades2026/pet-game.py
@@ -0,0 +1,10 @@
+# Part 1: Variables and Sequence
+pet_name = input("What is your pet's name?")
+health = 10
+hunger = 5
+energy = 10
+
+print(pet_name + " has arrived!")
+print("Health:", health)
+print("Hunger:", hunger)
+print("Energy:", energy)
